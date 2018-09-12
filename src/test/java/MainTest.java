b01ebe19e757a681723561a850b207750988f47e
jemmyZheng <jemmy.zheng@hotmail.com>
2018-09-12 10:34:12 +0800

    fix: remove the old test code

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,7 +2,7 @@
 public class MainTest {
   public static void main(String[] args) {
     // TODO
-    // don't worry ,this sdk we have been test lots of times in our payment demo.
+    // don't worry ,this sdk we have been test lots of times in our payment demo. ^_^
     return;
   }
 }
