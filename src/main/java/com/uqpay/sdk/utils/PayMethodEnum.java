c3f031436cc56a65ac97d52fce842f401eb82a22
jemmyZheng <jemmy.zheng@hotmail.com>
2018-11-29 17:18:43 +0800

    fix: add union pay offline qr

--- src/main/java/com/uqpay/sdk/utils/PayMethodEnum.java
+++ src/main/java/com/uqpay/sdk/utils/PayMethodEnum.java
@@ -8,6 +8,7 @@ package com.uqpay.sdk.utils;
  */
 public enum PayMethodEnum {
   UnionPayQR(PayMethod.UnionPayQR, ScenesEnum.QRCode),
+  UnionPayOfflineQR(PayMethod.UnionPayOfflineQR, ScenesEnum.QRCode),
   AlipayQR(PayMethod.AlipayQR, ScenesEnum.QRCode),
   WeChatQR(PayMethod.WeChatQR, ScenesEnum.QRCode),
   WechatWebBasedInApp(PayMethod.WechatWebBasedInApp, ScenesEnum.RedirectPay),
