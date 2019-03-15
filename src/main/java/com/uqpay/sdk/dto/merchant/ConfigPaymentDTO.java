852246f9d4ee12308fcfbdba58163666ff04155c
jemmyZheng <jemmy.zheng@hotmail.com>
2019-03-15 14:49:50 +0800

    fix: rm currency required on payment config

--- src/main/java/com/uqpay/sdk/dto/merchant/ConfigPaymentDTO.java
+++ src/main/java/com/uqpay/sdk/dto/merchant/ConfigPaymentDTO.java
@@ -9,10 +9,7 @@ public class ConfigPaymentDTO extends BaseJsonRequestDTO {
   private static final long serialVersionUID = -2581487655316468860L;
   @Positive
   private int methodId;
-  @NotEmpty
-  private String tradeCurrency;
-  @NotEmpty
-  private String settleCurrency;
+
   private int feeTempID; // fee template id, u can set a fee template on UQPAY Partner Dashboard;
   private boolean defOpen;// if true, it will be active automatic
 
@@ -22,22 +19,6 @@ public class ConfigPaymentDTO extends BaseJsonRequestDTO {
 
   public void setMethodId(int methodId) {
     this.methodId = methodId;
-  }
-
-  public String getTradeCurrency() {
-    return tradeCurrency;
-  }
-
-  public void setTradeCurrency(String tradeCurrency) {
-    this.tradeCurrency = tradeCurrency;
-  }
-
-  public String getSettleCurrency() {
-    return settleCurrency;
-  }
-
-  public void setSettleCurrency(String settleCurrency) {
-    this.settleCurrency = settleCurrency;
   }
 
   public int getFeeTempID() {
