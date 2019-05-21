2cbb44a6ee73cc2cdab5f4bf51a85d98bcdbee0a
jemmyZheng <jemmy.zheng@hotmail.com>
2019-05-21 15:17:23 +0800

    fix: query api support query with merchant order id

--- src/main/java/com/uqpay/sdk/dto/operation/OrderQuery.java
+++ src/main/java/com/uqpay/sdk/dto/operation/OrderQuery.java
@@ -6,8 +6,10 @@ import com.uqpay.sdk.utils.Constants;
 import com.uqpay.sdk.utils.enums.SignTypeEnum;
 import com.uqpay.sdk.utils.enums.UqpayTransType;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.Date;
 
 public class OrderQuery implements PaygateParams {
@@ -18,13 +20,29 @@ public class OrderQuery implements PaygateParams {
   @ParamLink(Constants.PAY_OPTIONS_TRADE_TYPE)
   @NotNull
   private UqpayTransType tradeType = UqpayTransType.query;
-  @Positive
-  @ParamLink(Constants.RESULT_UQPAY_ORDER_ID)
-  private long uqOrderId; // the uqpay order id
 
   @ParamLink(Constants.ORDER_DATE)
   @NotNull
   private Date date; // this request generate date
+
+  /**
+   * required rule: orderId not empty || uqOrderId > 0
+   */
+
+  @ParamLink(Constants.ORDER_ID)
+  private String orderId; // your origin order id
+
+  @PositiveOrZero
+  @ParamLink(Constants.RESULT_UQPAY_ORDER_ID)
+  private long uqOrderId; // the uqpay order id
+
+  public String getOrderId() {
+    return orderId;
+  }
+
+  public void setOrderId(String orderId) {
+    this.orderId = orderId;
+  }
 
   public UqpayTransType getTradeType() {
     return tradeType;
